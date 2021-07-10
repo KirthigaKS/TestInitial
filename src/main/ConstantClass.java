76c45a9550cc1973b6a467edc2295d8d9a87a4e9
KirthigaKS <87214441+KirthigaKS@users.noreply.github.com>
2021-07-10 07:31:12 +0530

    Test
    Test

---
+++ src/main/ConstantClass.java
@@ -0,0 +1,8 @@
+package main;
+
+public class ConstantClass {
+	
+	
+public static String searchbox="//input[@class='gLFyf gsfi']";
+public static String link="//a[contains(text(),'How Search works')]";
+}
